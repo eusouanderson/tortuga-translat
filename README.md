4e8ab16d821a7946cd69955d18fee90ae47e27e0
Anderson B.O.B <55605767+eusouanderson@users.noreply.github.com>
2023-05-14 12:28:15 -0300

    "update"

--- README.md
+++ README.md
@@ -1,15 +1,52 @@
+# Tortuga-translate
 
-# tortuga-translate
-~~~~
 Universal Translation Program
-~~~~
-## Tortuga Translate is a GUI creation with Tkinter and Canvas
+Tortuga Translate is a GUI creation with Tkinter and Canvas
 
 
-### Used the google translate api
+## Ferramentas
 
-~~~~
-    translates language to PTBR
-~~~~
+1. Linguagem: [Python](https://www.python.org/) 
+2. Framework: [Tkinter](https://docs.python.org/3/library/tkinter.html) [Canvas](https://pythonbasics.org/tkinter-canvas/)
+3. Plataforma de Hospedagem: 
 
-### Version for Windows 10
+## Get Started
+
+1. Baixe o arquivo app.py junto com seu código
+2. Adicionar as libs necessárias para o seu projeto no arquivo [requirements.txt](./requirements.txt)
+3. Estou usando Linux Ubuntu 22.04.2 LTS
+
+## Testar Local
+
+Para testar local execute o seguinte comando
+no diretorio :
+
+
+
+No Linux :
+
+~~~
+ python3 app.py run
+~~~
+
+No Windows :
+
+~~~
+ python app.py run
+~~~
+
+## Testar em Rede
+Para testar em rede execute o seguinte comando
+no diretorio :
+
+## Instruções
+
+No input digite o texto que gostaria de traduzir e clique no butão translate .
+
+
+<p align="center">
+    <img width="500" heigth="500" src="./img/tortuga.gif">
+
+
+---
+Developed by [Anderson B.O.B](https://github.com/eusouanderson)
---
+++ app.py
@@ -0,0 +1,80 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from time import sleep
+from googletrans import Translator
+
+app = Tk()
+app.overrideredirect(False)
+app.title('Tortuga Translat')
+app.geometry('756x560')
+"""app2 = Toplevel(app)"""
+
+msg = StringVar()
+
+# Quit Software
+def quitSoftware(event):
+    sleep(0.2)
+    app.destroy()
+
+# Button Translat
+def button_translat(event):
+    translat(msg.get())
+
+# Api google
+def translat(txt):
+
+    """Used version pip3 install googletrans==3.1.0a0"""
+    translator = Translator()
+    translation = translator.translate(txt, dest='pt')
+    msg1 = f'{translation.origin} origin ({translation.src})\n ' \
+           f'translat to ({translation.dest}) {translation.text}\n'
+    labelText.config(labelText, text=msg1.upper())
+
+
+# Canvas init
+canvas = Canvas(
+    app, width=756, height=560, bd=0, highlightthickness=0, relief='ridge'
+)
+canvas.pack()
+bgImg = ImageTk.PhotoImage(Image.open('img/Layout.png'))
+bg = canvas.create_image(0, 0, image=bgImg, anchor=NW)
+
+# Button Exit
+buttonExImg = ImageTk.PhotoImage(Image.open('img/ButtonExit.png'))
+buttonExit = canvas.create_image(50, 500, image=buttonExImg)
+canvas.tag_bind(buttonExit, '<Button-1>', quitSoftware)
+
+# Button Translate
+buttonTranslateImg = ImageTk.PhotoImage(Image.open('img/ButtonTranslate.png'))
+buttonTranslate = canvas.create_image(400, 250, image=buttonTranslateImg)
+canvas.tag_bind(buttonTranslate, '<Button-1>', button_translat)
+
+# Entry
+entryImg = ImageTk.PhotoImage(Image.open('img/Entry.png'))
+entryIm = canvas.create_image(400, 100, image=entryImg)
+entry = Entry(
+    app,
+    font=('Helvetica', 24),
+    width=8,
+    textvariable=msg,
+    bg='#000000',
+    fg='#F2F2F2',
+    bd=0,
+    highlightcolor='blue',
+)
+entryy = canvas.create_window(
+    200, 56, anchor='nw', window=entry, width=400, height=85
+)
+
+# Label Text
+labelText = Label(
+    app,
+    font=('Hevetica', 15),
+    bg='#000000',
+    fg='#F2F2F2',
+    highlightcolor='black',
+)
+labelText1 = canvas.create_window(10, 356, anchor='nw', window=labelText)
+
+
+app.mainloop()
